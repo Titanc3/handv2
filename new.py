373ff7daa2b3112ef4389fb34e2327a8bb1b354c
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2026-07-29 02:58:52 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,41 +1,30 @@
-"""
-This ESP32 MicroPython code was developed by newbiely.com
-This ESP32 MicroPython code is made available for public use without any restriction
-For comprehensive instructions and wiring diagrams, please visit:
-https://newbiely.com/tutorials/esp32-micropython/esp32-micropython-oled-128x64
-"""
-
 from machine import I2C, Pin
 from DIYables_MicroPython_OLED import OLED_SSD1306_I2C
-import bitmaps
+from bitmaps import clear, show, draw_bg
+import utime
 
-
-# Initialize I2C
 i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=400000)  # Adjust ESP32 pins according to your setup
 
-# Initialize the OLED display
 oled = OLED_SSD1306_I2C(128, 64, i2c)
 
-# Clear the display
 oled.clear_display()
-oled.display()
 
 oled.set_text_size(1)
 
 # Print a message to the display
 
-devNum = 3
-text = f"{devNum:02}"+"/16|"
+devNum = 15
+state = 2
+adcData = "1:.43:.2:.7:.23:.9"
+t1 = f"{devNum+1:02}"+"/16" # text1
+t2 = "machine error"
+t3 = ":D"
 integer_value = 123
 float_value = 45.67
 
-oled.set_cursor(0, 0)
-oled.println(text)
-oled.set_cursor(0, 8)
-oled.println("-----*")
-oled.set_cursor(0, 25)
-oled.println(str(integer_value))  # Print integer and move to the next line
-oled.set_cursor(0, 50)
-oled.println("{:.2f}".format(float_value))  # Print formatted float and move to the next line
-oled.display()  # Ensure you update the display after writing to it
+def draw():
+    clear()
+    draw_bg(devNum, adcData, state, t1, t2, t3)
+    show()
 
+draw()
