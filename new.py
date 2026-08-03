19eddb8df0ef9398866c7993c3b0382e3a87604f
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2026-08-03 04:34:38 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,8 @@
-from machine import I2C, Pin # programmed by mark:D  (for esp32-c3 SuperMini)
+from machine import I2C, Pin, RTC # programmed by mark:D  (for esp32-c3 SuperMini)
 from DIYables_MicroPython_OLED import OLED_SSD1306_I2C
 from bitmaps import clear, show, draw_bg
 import utime
-from micropython_lsm6dsox import lsm6dsox
+from lsm6ds3 import LSM6DS3, NORMAL_MODE_104HZ
 
 import network
 import espnow
@@ -20,6 +20,8 @@ a.atten(ADC.ATTN_11DB)
 b.atten(ADC.ATTN_11DB)
 c.atten(ADC.ATTN_11DB)
 d.atten(ADC.ATTN_11DB)
+
+rtc = RTC() # time setup
 
 sta = network.WLAN(network.WLAN.IF_STA)  ##NETWORK SETUP##
 sta.active(True)
@@ -39,14 +41,69 @@ oled.clear_display()
 oled.set_text_size(1)
 # Print a message to the display
 
+sensor = LSM6DS3(i2c, mode=NORMAL_MODE_104HZ)
+
+
 state = 0
 adcData = "1:.43:.2:.7:.23:.9"
-t1 = f"{devNum+1: 2} " + pNames[devNum]
-t2 = ""
-t3 = ":D"
+time = rtc.datetime()
+print(time)
+battery = 14
+t1 = f" {time[4]%12:02}:{time[5]:02} {battery:02}%"
+t2 = f"{devNum+1:02} " + pNames[devNum]
+t3 = ":D\ng"
+devIcon = [0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x02, 0x00, 0x00, 0x00, 
+0x06, 0x00, 0x40, 0x00, 
+0x04, 0x00, 0x40, 0x00, 
+0x04, 0x00, 0x40, 0x00, 
+0x04, 0x00, 0x40, 0x00, 
+0x04, 0x00, 0x40, 0x00, 
+0x04, 0x00, 0x20, 0x00, 
+0x04, 0x00, 0x20, 0x00, 
+0x04, 0x00, 0x20, 0x00, 
+0x04, 0x00, 0x20, 0x00, 
+0x06, 0x00, 0x20, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x02, 0x00, 0x00, 0x00, 
+0x02, 0x00, 0x00, 0x00, 
+0x02, 0x00, 0x60, 0x00, 
+0x02, 0x00, 0x40, 0x00, 
+0x01, 0x00, 0x80, 0x00, 
+0x01, 0x01, 0x00, 0x00, 
+0x01, 0x01, 0x00, 0x00, 
+0x00, 0x81, 0x00, 0x00, 
+0x00, 0x82, 0x00, 0x00, 
+0x00, 0x4c, 0x00, 0x00, 
+0x00, 0x30, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x04, 0x00, 
+0x00, 0x00, 0x45, 0x00, 
+0x00, 0x00, 0x4c, 0x00, 
+0x00, 0x00, 0x74, 0x80, 
+0x00, 0x00, 0x44, 0x80, 
+0x00, 0x00, 0x44, 0x80, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00, 
+0x00, 0x00, 0x00, 0x00]
 
 handAdj = [3500, 3500, 3500, 3500] #############################################################ADJUST THRESHOLDS HERE#####################################################################
 joystickAdj = [3500, 3500, 3500, 3500]
+
 
 def clamp(n, minn, maxn): # generic func
     return max(min(maxn, n), minn)
@@ -69,14 +126,17 @@ def poll(): # polls hand then joystick
         return [[A1, B1, C1, D1], [A2, B2, C2, D2]]
 
 def draw():
+    t1 = f" {time[4]%12:02}:{time[5]:02} {battery:02}%"
+    t2 = f"{devNum+1:02} " + pNames[devNum]
     clear()
-    draw_bg(devNum, adcData, state, t1, t2, t3)
+    draw_bg(devNum, adcData, state, t1, t2, t3, battery, devIcon)
     show()
 
 def toggle_connect(): # this pauses the poll->send cycle
     global state
     if state == 0:
         espn.add_peer(pList[devNum])
+        espn.send("send icon pls :D")
         state = 1
         draw()
         exitnum = 0
@@ -84,6 +144,7 @@ def toggle_connect(): # this pauses the poll->send cycle
         if msg:             # msg == None if timeout in recv()
             if host == pList[devNum]:
                 state = 3
+                devIcon = msg
         state -= 1 # sneaky way to ensure state 0 or 2 based on connection
     else:
         espn.del_peer(pL[pIndex])
@@ -99,3 +160,10 @@ draw()
 toggle_connect()
 draw()
 
+ax, ay, az, gx, gy, gz = sensor.get_readings()
+print("Accelerometer\nX:{}, Y:{}, Z:{}\nGyro\nX:{}, Y:{}, Z{}\n\n ".format(ax, ay, az, gx, gy, gz))
+
+for x in range(0, 16):
+    devNum = x
+    t1 = f" {time[4]%12:02}:{time[5]:02} {battery:02}%"
+    draw()
