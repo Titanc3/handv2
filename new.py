75a61344b1c8e0d55fb7cce7cf14576385584e6b
Titanc3 <123788674+Titanc3@users.noreply.github.com>
2026-08-03 01:42:42 -0400

    Update new.py

--- new.py
+++ new.py
@@ -2,12 +2,16 @@ from machine import I2C, Pin # programmed by mark:D  (for esp32-c3 SuperMini)
 from DIYables_MicroPython_OLED import OLED_SSD1306_I2C
 from bitmaps import clear, show, draw_bg
 import utime
+from micropython_lsm6dsox import lsm6dsox
 
 import network
 import espnow
 from machine import Pin, ADC, sleep, PWM
 
-adcEn = Pin(2, Pin.OUT) ##ADC SETUP##     high to enable joystick, low to enable finger flexers
+fingerEn = Pin(2, Pin.OUT) ##ADC SETUP##    low to enable finger flexers
+joystickEn = Pin(21, Pin.OUT)
+fingerEn.value(1)
+joystickEn.value(1)
 a = ADC(3) # pinky finger                     joystick x
 b = ADC(4) # ring finger                      joystick y
 c = ADC(1) # middle finger                    joystick button
@@ -48,16 +52,20 @@ def clamp(n, minn, maxn): # generic func
     return max(min(maxn, n), minn)
 
 def poll(): # polls hand then joystick
-        adcEn.value(0)# hand poll
+        fingerEn.value(0)
+        joystickEn.value(1)
         A1 = clamp(a.read()/handAdj[0], 0, 1) # convert to % flex
         B1 = clamp(b.read()/handAdj[1], 0, 1)
         C1 = clamp(c.read()/handAdj[2], 0, 1)
         D1 = clamp(d.read()/handAdj[3], 0, 1)
-        adcEn.value(1)# joystick poll
+        fingerEn.value(1)
+        joystickEn.value(0)
         A2 = clamp(a.read()/joystickAdj[0], 0, 1) # convert to % flex=
         B2 = clamp(b.read()/joystickAdj[1], 0, 1)
         C2 = clamp(c.read()/joystickAdj[2], 0, 1)
         D2 = clamp(d.read()/joystickAdj[3], 0, 1)
+        fingerEn.value(1)
+        joystickEn.value(1)
         return [[A1, B1, C1, D1], [A2, B2, C2, D2]]
 
 def draw():
@@ -90,3 +98,4 @@ print(poll())
 draw()
 toggle_connect()
 draw()
+
